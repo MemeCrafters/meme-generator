ef0ad8485691f7a3167a2ce693f1db3bc8658d59
meetwq <meetwq@gmail.com>
2024-12-31 17:03:28 +0800

    adjust

--- meme_generator/memes/your_new_years_eve/__init__.py
+++ meme_generator/memes/your_new_years_eve/__init__.py
@@ -4,15 +4,23 @@ from pathlib import Path
 from pil_utils import BuildImage
 
 from meme_generator import add_meme
+from meme_generator.utils import make_jpg_or_gif
 
 img_dir = Path(__file__).parent / "images"
 
 
 def your_new_years_eve(images: list[BuildImage], texts: list[str], args):
     frame = BuildImage.open(img_dir / "0.png")
-    img = images[0].convert("RGBA").resize((586,430),inside=True,keep_ratio=True)
-    frame.paste(img,(0,650),alpha=True,below=True)
-    return frame.save_jpg()
+
+    def make(imgs: list[BuildImage]) -> BuildImage:
+        img = (
+            imgs[0]
+            .convert("RGBA")
+            .resize((586, 430), inside=True, keep_ratio=True, bg_color="white")
+        )
+        return frame.copy().paste(img, (0, 650), alpha=True, below=True)
+
+    return make_jpg_or_gif(images, make)
 
 
 add_meme(
@@ -21,6 +29,6 @@ add_meme(
     min_images=1,
     max_images=1,
     keywords=["你的跨年"],
-    date_created=datetime(2024,12,31),
-    date_modified=datetime(2024,12,31),
+    date_created=datetime(2024, 12, 31),
+    date_modified=datetime(2024, 12, 31),
 )
