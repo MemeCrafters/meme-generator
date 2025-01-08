be6fb7afeafa2f77b0c8bc321d8c452a6dc76822
meetwq <meetwq@gmail.com>
2025-01-08 16:08:51 +0800

    adjust

--- meme_generator/memes/luotianyi_say/__init__.py
+++ meme_generator/memes/luotianyi_say/__init__.py
@@ -5,41 +5,40 @@ from pil_utils import BuildImage
 
 from meme_generator import add_meme
 from meme_generator.exception import TextOverLength
-from meme_generator.utils import make_jpg_or_gif
+from meme_generator.tags import MemeTags
 
 img_dir = Path(__file__).parent / "images"
 
 default_text = "好想去海边啊～"
 
 
-def lty(images: list[BuildImage],texts: list[str], args):
+def luotianyi_say(images: list[BuildImage], texts: list[str], args):
     frame = BuildImage.open(img_dir / "0.png")
-    text = texts[0] if texts else default_text
+    text = texts[0]
     try:
         frame.draw_text(
-            (520, frame.height - 1054, frame.width - 20, 184),
+            (520, 20, frame.width - 20, 220),
             text,
-            min_fontsize=64,
+            min_fontsize=40,
             max_fontsize=140,
-            fill=(102,204,255),
+            fill="#66CCFF",
             allow_wrap=True,
             lines_align="center",
         )
     except ValueError:
         raise TextOverLength(text)
-    
+
     return frame.save_jpg()
 
 
 add_meme(
-    "lty",
-    lty,
-    min_images=0,
-    max_images=0,
-    min_texts=0,
+    "luotianyi_say",
+    luotianyi_say,
+    min_texts=1,
     max_texts=1,
     default_texts=[default_text],
-    keywords=["天依say", "天依生","天依说"],
+    keywords=["洛天依说", "天依说"],
+    tags=MemeTags.luotianyi,
     date_created=datetime(2025, 1, 7),
     date_modified=datetime(2025, 1, 7),
 )
