f6f318f18adf7f012d8d6ec7df5f2fb9d1896e18
電撃メロン <lxh3058330282@gmail.com>
2025-01-19 22:54:43 +0800

    rewrite kokona_say

---
+++ meme_generator/memes/ba_say/__init__.py
@@ -0,0 +1,149 @@
+import random
+from dataclasses import dataclass
+from datetime import datetime
+from pathlib import Path
+from typing import Literal
+
+from arclet.alconna import store_value
+from pil_utils import BuildImage
+from pydantic import Field
+from PIL.Image import Transpose
+
+from meme_generator import (
+    MemeArgsModel,
+    MemeArgsType,
+    ParserArg,
+    ParserOption,
+    add_meme,
+    CommandShortcut,
+)
+from meme_generator.exception import TextOverLength,MemeFeedback
+from meme_generator.tags import MemeTags
+
+img_dir = Path(__file__).parent / "images"
+
+
+@dataclass
+class Character:
+    name_cn:str
+    name_en:str
+
+Characters = [
+    Character("爱丽丝", "arisu"),
+    Character("泉奈", "izuna"),
+    Character("key", "key"),
+    Character("心奈", "kokona"),
+    Character("玛丽", "mari"),
+    Character("小夏", "natsu"),
+    Character("玲纱", "reisa"),
+    Character("濑名", "sena"),
+    Character("优香", "yuuka"),
+]
+
+
+help_text = [
+    "角色编号："+"，".join(
+        [f"{i+1}、{Characters[i].name_cn}" for i in range(9)]
+    ),
+    "消息框的位置，包含 left、right、random"
+    ]
+
+
+class Model(MemeArgsModel):
+    character: int = Field(
+        0, description=help_text[0]
+    )
+    position: Literal["left", "right","random"] = Field(
+        "random", description=help_text[1]
+    )
+
+
+args_type = MemeArgsType(
+    args_model=Model,
+    args_examples=[Model(character=c,position="right") for c in range(1,10)],
+    parser_options=[
+        ParserOption(
+            names=["-c", "--character"],
+            args=[ParserArg(name="character", value="str")],
+            help_text=help_text[0],
+        ),
+        ParserOption(
+            names=["-p", "--position"],
+            args=[ParserArg(name="position", value="str")],
+            help_text=help_text[1],
+        ),
+        ParserOption(
+            names=["--left", "左"], dest="position", action=store_value("left")
+        ),
+        ParserOption(
+            names=["--right", "右"], dest="position", action=store_value("right")
+        )
+    ],
+)
+
+
+
+def ba_say(images, texts: list[str], args: Model):
+    text = texts[0]
+
+    if args.character == 0:
+        character = random.choice(Characters)
+    elif args.character <= 9:
+        character = Characters[args.character-1]
+    else:
+        raise MemeFeedback("角色编号错误，请输入1-9")
+
+    if args.position in ["left", "right"]:
+        position = args.position
+    else:
+        position = random.choice(["left", "right"])
+
+    xy = (60, 0, 580, 200) if position == "left" else (500, 0, 1020, 200)
+
+    frame = BuildImage.open(img_dir / f"{character.name_en}.png")
+    if position == "left":
+        frame = frame.transpose(Transpose.FLIP_LEFT_RIGHT)
+
+    try:
+        frame.draw_text(
+            xy,
+            text,
+            allow_wrap=True,
+            max_fontsize=100,
+            min_fontsize=10,
+            fill="black",
+            lines_align="center",
+        )
+    except TextOverLength:
+        raise TextOverLength(text)
+
+    return frame.save_png()
+
+
+add_meme(
+    "ba_say",
+    ba_say,
+    min_texts=0,
+    max_texts=1,
+    default_texts=["那我问你"],
+    args_type=args_type,
+    keywords=["ba说"],
+    shortcuts=[
+        CommandShortcut(
+            key=f"{Characters[i].name_cn}说",
+            args=["--character",f"{i+1}"]
+        )
+        for i in range(9)
+        ],
+    tags=MemeTags.arisu
+    | MemeTags.izuna
+    | MemeTags.key
+    | MemeTags.kokona
+    | MemeTags.mari
+    | MemeTags.natsu
+    | MemeTags.reisa
+    | MemeTags.sena
+    | MemeTags.yuuka,
+    date_created=datetime(2024, 12, 12),
+    date_modified=datetime(2025, 1, 19),
+)
